1644148b730e99a93730508bab98216c891e7918
unknown <dimassetyonugroho30@gmail.com>
2023-09-25 18:53:13 +0700

    perubahan baru coy

--- test456.java
+++ test456.java
@@ -2,6 +2,7 @@ package test456;
 
 public class test456 {
     public static void main(String[] args) {
-        System.out.println("belajar github 2");
+        String a = "KU SUKA QTELA";
+        System.out.println(a);
     }
 }
